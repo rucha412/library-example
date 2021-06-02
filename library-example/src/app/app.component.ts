2dfea56cc3d02000851cbb57613a55a4e4895fcc
rucha412 <ruchadeshpande04@gmail.com>
2021-06-03 00:03:47 +0530

    Integrated CryptoJS and Moment

---
+++ library-example/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import moment from 'moment';
+import * as CryptoJS from 'crypto-js';
+import { Constants } from './constants';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'library-example';
+  date = moment().format('MMM Do YY');
+  ciphertext = CryptoJS.AES.encrypt(
+    Constants.message,
+    Constants.key
+  ).toString();
+
+  // Decrypt
+  bytes = CryptoJS.AES.decrypt(this.ciphertext, Constants.key);
+  originalText = this.bytes.toString(CryptoJS.enc.Utf8);
+}
---
+++ library-example/src/app/constants.ts
@@ -0,0 +1,4 @@
+export const Constants = {
+  key: 'ABCDEFGH',
+  message: 'Hello!!'
+};
